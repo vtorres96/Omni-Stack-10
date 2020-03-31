c677aa8efe6f8e46c2b2b4f9d4316dbf9c9eecc1
vtorres96 <victorcdc96@gmail.com>
2020-03-31 20:36:35 -0300

    added useCreateIndex in mongoose connect

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ const server = http.Server(app);
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-bfkrt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   {
+    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
