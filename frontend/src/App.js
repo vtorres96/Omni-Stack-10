e9241a06eda4e382384b0699ef2ce1b4c7f22592
vtorres96 <victorcdc96@gmail.com>
2020-04-05 21:12:32 -0300

    added App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './assets/css/global.css';
+import './assets/css/app.css';
+import './assets/css/sidebar.css';
+import './assets/css/main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
-    <div className="App">
-    
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
