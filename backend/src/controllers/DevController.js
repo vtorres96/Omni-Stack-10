5053041381d5d19a8f4a94aa7099694a9fc00a7c
Victor Torres <victor.moreno@hypnobox.com.br>
2020-04-07 21:41:05 -0300

    added functions in controller

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
 
